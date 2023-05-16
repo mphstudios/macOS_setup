1fc0a706025c5762e5f591a1eb9c13be7a5d6e63
mph <mhrudka@getty.edu>
2023-05-15 17:26:55 -0700

    Move Homebrew bundle files to a subdirectory
    Rename base brewfile

--- functions/homebrew.sh
+++ functions/homebrew.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-# Install packages from a Homebrew bundle file
+# Install packages from brewfiles
 function homebrew_install_bundles {
-  install_bundle 'Brewfile'
+  # install packages from base bundle
+  install_bundle 'brewfiles/base'
 
-  read -r -p "Install Fonts? [Yn]"
+  read -r -p "Install cask applications bundle? [Yn]"
   if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]];
   then
-    install_bundle 'Caskfile'
+    install_bundle 'brewfiles/casks'
   fi
 
-  read -r -p "Install Fonts? [Yn]"
+  read -r -p "Install cask fonts bundle? [Yn]"
   if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]];
   then
-    install_bundle 'CaskFonts'
+    install_bundle 'brewfiles/fonts'
+  fi
+
+  read -r -p "Install Mac App Store bundle? [Yn]"
+  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]];
+  then
+    install_bundle 'brewfiles/mas'
   fi
 }
 
@@ -46,7 +53,7 @@ function homebrew_install_package {
 }
 
 function install_bundle {
-  local bundle_file=$1
+  local brewfile=$1
 
   if test ! $(which brew); then
     install_homebrew
@@ -55,14 +62,14 @@ function install_bundle {
   # ensure that node and npm install properly via Homebrew
   unset -v NODE_PATH
 
-  message "Installing Homebrew packages from $bundle_file"
+  message "Installing Homebrew packages from $brewfile"
 
   brew update
   brew upgrade
 
-  download bundle_file
+  download brewfile
 
-  brew bundle --file=$SRC_DIR/$bundle_file --require-sha
+  brew bundle --file=$brewfile --require-sha
 
   message "Removing cached Homebrew downloads..."
   brew cleanup
