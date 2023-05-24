98d0c32b1abee9ff0a0667864d37e53b1f59a7c4
mph <mhrudka@getty.edu>
2023-05-23 22:03:53 -0700

    Fix error in sublime_text script

--- functions/sublime_text.sh
+++ functions/sublime_text.sh
@@ -6,9 +6,8 @@ function install_subl_command {
 }
 
 function symlink_sublime_settings {
-    local dropbox_dir="$HOME/Dropbox/Application\ Support/"
-    local settings_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-    [ ! -L "$settings_dir" ] && rm -rf settings_dir
-    ln -sfn dropbox_dir/Sublime\ Text/User settings_dir
-  }
+  local dropbox_dir="$HOME/Dropbox/Application\ Support/"
+  local settings_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+  [ ! -L "$settings_dir" ] && rm -rf settings_dir
+  ln -sfn dropbox_dir/Sublime\ Text/User settings_dir
 }
