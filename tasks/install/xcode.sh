a7c87cce7ae251713d0962b4793a4d06222c486c
mph <mph@mph.info>
2026-03-24 21:21:38 -0700

    Expand xcode-select -p option to --print-path

--- tasks/install/xcode.sh
+++ tasks/install/xcode.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 
 source "${MISE_PROJECT_DIR}/lib/output.sh"
 
-if xcode-select -p &>/dev/null; then
+if xcode-select --print-path &>/dev/null; then
   skip "Xcode CLT"
 else
   info "Installing Xcode Command Line Tools..."
   xcode-select --install
-  until xcode-select -p &>/dev/null; do
+  until xcode-select --print-path &>/dev/null; do
     sleep 5
   done
   ok "Xcode CLT installed"
