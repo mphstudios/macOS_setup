aaa9ed42fc615b182704159825379c2069adf258
mph <mph@mph.info>
2026-04-03 19:59:40 -0700

    Simplify Homebrew installer invocation: remove redundant bash wrapper

--- tasks/install/homebrew.sh
+++ tasks/install/homebrew.sh
@@ -12,7 +12,7 @@ if command -v brew &>/dev/null; then
 fi
 
 spin "Installing Homebrew..." \
-  bash -c '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+  bash -c 'curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash'
 
 # Persist Homebrew in PATH via /etc/paths.d (read by path_helper for all shells)
 # Only needed on Apple Silicon (/usr/local/bin is already in the default PATH on Intel)
